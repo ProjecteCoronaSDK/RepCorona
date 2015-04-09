e8404fea64558b788ea6f6a3569c6482679e0a32
pometa444 <andreaserrano43@gmail.com>
2015-04-09 19:52:33 +0100

    EntregaMusica

---
+++ MusicaJoc/main.lua
@@ -0,0 +1,9 @@
+
+local composer = require( "composer" )
+
+_centerX = display.contentCenterX
+_centerY = display.contentCenterY 
+_W = display.contentWidth 
+_H = display.actualContentHeight
+composer.gotoScene( "sceneMusicImage" )
+
