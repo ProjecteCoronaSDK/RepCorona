4fde0131b0af52f46a20502039012a778f75f8ae
pometa444 <andreaserrano43@gmail.com>
2015-04-15 00:04:20 +0200

    boletes

---
+++ main.lua
@@ -0,0 +1,12 @@
+
+local composer = require( "composer" )
+
+_centerX = display.contentCenterX
+_centerY = display.contentCenterY 
+_W = display.contentWidth 
+_H = display.actualContentHeight
+
+
+
+composer.gotoScene( "sceneComida" )
+
