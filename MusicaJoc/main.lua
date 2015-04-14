c2694c4bad36740bd915a5dedb4252838d93b548
pometa444 <andreaserrano43@gmail.com>
2015-04-15 00:03:33 +0200

    p

--- MusicaJoc/main.lua
+++
@@ -1,9 +0,0 @@
-
-local composer = require( "composer" )
-
-_centerX = display.contentCenterX
-_centerY = display.contentCenterY 
-_W = display.contentWidth 
-_H = display.actualContentHeight
-composer.gotoScene( "sceneMusicImage" )
-
